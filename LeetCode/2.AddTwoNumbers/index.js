7a89cc7e4bb74f0dd57ed41844680bccd265686f
Muyunyun <328375795@qq.com>
2018-08-07 14:44:45 +0800

    docs: leetcode 3

--- LeetCode/2.AddTwoNumbers/index.js
+++ LeetCode/2.AddTwoNumbers/index.js
@@ -1,30 +1,5 @@
-### Title
+// Keyword: single linked list
 
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
-### Example
-
-```
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-Explanation: 342 + 465 = 807.
-```
-
-### Analyze
-
-Pseudocode as follow:
-
-* 初始化 head, temp
-* 循环遍历两个链表
-  * 根据 sum 的值来决定是否要处理进位
-
-### Answer
-
-Keyword: single linked list
-
-```js
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -76,4 +51,3 @@ var addTwoNumbers = function(l1, l2) {
   }
   return head
 }
-```
