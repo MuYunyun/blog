e72b499b443c09e391832fc085a7928abfb5daba
MuYunyun <328375795@qq.com>
2020-02-17 22:09:04 +0800

    docs: some fn do with linked_list

---
+++ BasicSkill/LeetCode/helps/linked_list.js
@@ -0,0 +1,36 @@
+// some useful fn to do with linked_list
+
+/**
+ * create linked list
+ */
+var initList = (function createLinkedList(...args) {
+  function ListNode(val) {
+    this.val = val;
+    this.next = null;
+  }
+  const tmplistNode = new ListNode(0)
+  let length = args.length
+  let cur = new ListNode(args[length - 1])
+  tmplistNode.next = cur
+  while (length-- && length >= 1) {
+    let next = new ListNode(args[length - 1])
+    cur.next = next
+    cur = next
+  }
+
+  return tmplistNode.next
+})(1, 2, 3, 4, 5)
+
+/**
+ * print the linked list
+ */
+var printList = function (list) {
+  let cur = list
+  let str = ''
+  while (cur) {
+    str += `${cur.val} -> `;
+    cur = cur.next
+    cur === null && (str += 'null')
+  }
+  console.log(str)
+}
