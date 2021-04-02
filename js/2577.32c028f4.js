60d679f50bdcf90dcb603857ecea2a8e68f6671e
MuYunyun <MuYunyun@users.noreply.github.com>
2021-04-02 05:02:53 +0000

    deploy: abc2dc8538ffa914481fa641de8670a6decf04e0

---
+++ js/2577.32c028f4.js
@@ -0,0 +1,1 @@
+(self.webpackChunkblog=self.webpackChunkblog||[]).push([[2577],{12577:n=>{n.exports="### A/B 测试\n\nAB 测试是一个反复迭代优化的过程，它的基本步骤如下图所示可以划分为\n\n![](http://with.muyunyun.cn/036ddf6dae41356524211c8bdf830744.jpg)\n"}}]);
